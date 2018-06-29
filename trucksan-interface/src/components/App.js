8c2b511c5f13e0a84a6b44a0ac9ebcd291763032
lebrancconvas <lebrancconvas@gmail.com>
2018-06-29 07:42:33 +0700

    Project Updated

--- trucksan-interface/src/components/App.js
+++ trucksan-interface/src/components/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from '../img/logo.svg';
+import Header from './Header.js';
+import '../App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App-header">
-        <h1>Trucksan Delivery Company</h1>
+      <div>
+        <Header />
       </div>
     );
   }
---
+++ trucksan-interface/src/components/Header.js
@@ -0,0 +1,17 @@
+import React, {
+    Component
+} from 'react';
+import logo from '../img/logo.svg';
+import '../App.css';
+
+class Header extends Component {
+    render() {
+        return ( 
+            <div className = "App-header">
+                <h1>Trucksan Delivery Company</h1> 
+            </div>
+        );
+    }
+}
+
+export default Header;
